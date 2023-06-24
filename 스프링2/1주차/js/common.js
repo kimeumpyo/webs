4fe50c4f13b2ae40a0f5a99f181c65576311ff68
kimeumpyo <eumpyo0409@gmail.com>
2023-06-24 17:43:01 +0900

    web

---
+++ 스프링2/1주차/js/common.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("button");
+console.log(button);
+
+window.addEventListener("DOMContentLoaded", function(){
+    // document 객체 변환 완료
+    const button = document.getElementById("button");
+    console.log(button);
+})
