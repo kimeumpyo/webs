244ec6bcaa61c9c7c4f49ad3acf88601cb2b93ad
Gtimek <wnsdnjs111625@gmail.com>
2023-08-26 15:44:15 +0900

    web

--- 스프링3/exam02/src/main/java/config/ControllerConfig.java
+++ 스프링3/exam02/src/main/java/config/ControllerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 // 하위에 있는 모든 파일을 스캔한다
 @ComponentScan({"controllers", "models"})
 public class ControllerConfig {
+    
     /*
     @Bean
     public HelloController helloController(){
